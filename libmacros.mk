fe2dd20e747ba01150f1bbfc9238877821b957f9
mpiau <contact@melvynpiau.com>
2026-01-10 13:50:21 +0000

    Protect .mk file from multiple inclusions if used multiple times as a dependency.

--- libmacros.mk
+++ libmacros.mk
@@ -1,4 +1,10 @@
 # Simply include this .mk file in your Makefile to use that library.
 
+# Avoid being included multiple times if used multiple times as a dependency. 
+ifndef LIBMACROS_INCLUDE
+LIBMACROS_INCLUDE := 1
+
 LIBMACROS_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 CFLAGS += -I$(LIBMACROS_DIR)include
+
+endif 
